77dbbf9c25c5ff119f06776cdfb5f3ebb32d02cb
Josh Curry <joshuacurry2615@gmail.com>
2021-10-04 10:28:54 -0400

    Initial Commit

---
+++ MapKitExploration/MapKitExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitExplorationApp.swift
+//  MapKitExploration
+//
+//  Created by Joshua Curry on 10/4/21.
+//
+
+import SwiftUI
+
+@main
+struct MapKitExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
