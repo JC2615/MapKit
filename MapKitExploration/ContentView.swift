39c4c8430ab8d8d10e4d22d3a72c42ce3f71707d
Josh Curry <joshuacurry2615@gmail.com>
2021-10-04 11:56:07 -0400

    - Added map annotations

--- MapKitExploration/ContentView.swift
+++ MapKitExploration/ContentView.swift
@@ -8,7 +8,19 @@
 import SwiftUI
 import MapKit
 
+struct City: Identifiable {
+    let id = UUID()
+    let coordinate: CLLocationCoordinate2D
+}
+
 struct MapExample: View {
+    
+    @State private var cities: [City] = [
+        City(coordinate: .init(latitude: 40.7128, longitude: 74.0060)),
+        City(coordinate: .init(latitude: 37.7749, longitude: 122.4194)),
+        City(coordinate: .init(latitude: 47.6062, longitude: 122.3321))
+    ]
+    
     @ObservedObject private var locationManager = LocationViewModel()
     @State private var userTrackingMode: MapUserTrackingMode = .follow
     @State private var region = MKCoordinateRegion(
@@ -28,8 +40,19 @@ struct MapExample: View {
                 coordinateRegion: $region,
                 interactionModes: MapInteractionModes.all,
                 showsUserLocation: true,
-                userTrackingMode: $userTrackingMode
-            )
+                userTrackingMode: $userTrackingMode,
+                annotationItems: cities
+            ) { city in
+                MapAnnotation(
+                    coordinate: city.coordinate,
+                    anchorPoint: CGPoint(x: 0.5, y: 0.5)
+                ) {
+                    Circle()
+                        .stroke(Color.green)
+                        .frame(width: 44, height: 44)
+                        //.background(Color.green)
+                }
+            }
                 .ignoresSafeArea()
             Button("zoom") {
                 withAnimation {
